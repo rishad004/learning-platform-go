d1cf35c1a443ab215fdd383bbf7c729ac7845594
rishad004 <rishadmuthu004@gmail.com>
2024-08-07 10:43:25 +0530

    port set for user-service

--- user-service/cmd/main.go
+++ user-service/cmd/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"log"
+	"net"
+
 	grpchandlers "github.com/rishad004/learning-platform-go/user-service/internal/delivery/grpc-handlers"
 	"github.com/rishad004/learning-platform-go/user-service/internal/repository"
 	"github.com/rishad004/learning-platform-go/user-service/internal/service"
 	"github.com/rishad004/learning-platform-go/user-service/pkg"
 	pb "github.com/rishad004/learning-platform-go/user-service/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -19,4 +23,16 @@ func main() {
 
 	g := grpc.NewServer()
 	pb.RegisterUserServiceServer(g, userHandler)
+
+	reflection.Register(g)
+
+	listen, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal("failed to listen, err: ", err)
+	}
+
+	log.Println("Server listening on port :8081")
+	if err := g.Serve(listen); err != nil {
+		log.Fatal("failed to listen - err: ", err)
+	}
 }
