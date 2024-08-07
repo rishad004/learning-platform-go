f6255f9678f0ad930ceadad53abc42d0155a383c
rishad004 <rishadmuthu004@gmail.com>
2024-08-07 14:21:59 +0530

    env load added

--- user-service/go.mod
+++ user-service/go.mod
@@ -41,6 +41,7 @@ require (
 
 require (
 	github.com/gin-gonic/gin v1.10.0
+	github.com/joho/godotenv v1.5.1
 	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
--- user-service/pkg/database.go
+++ user-service/pkg/database.go
@@ -3,14 +3,22 @@ package pkg
 import (
 	"os"
 
+	"github.com/joho/godotenv"
+	"github.com/rishad004/learning-platform-go/user-service/internal/model"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func InitDatabase() *gorm.DB {
+	if err := godotenv.Load("../.env"); err != nil {
+		panic(err)
+	}
+
 	Db, err := gorm.Open(postgres.Open(os.Getenv("DB_URL")))
 	if err != nil {
 		panic(err)
 	}
+
+	Db.AutoMigrate(&model.Users{})
 	return Db
 }
