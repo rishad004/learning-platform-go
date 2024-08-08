b7393eee1e26ea742feb981d0937e227f1516bca
rishad004 <rishadmuthu004@gmail.com>
2024-08-08 10:22:14 +0530

    user-service grpc and http goroutine setup done

--- user-service/cmd/main.go
+++ user-service/cmd/main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/gin-gonic/gin"
 	grpchandlers "github.com/rishad004/learning-platform-go/user-service/internal/delivery/grpc-handlers"
+	httpcontrollers "github.com/rishad004/learning-platform-go/user-service/internal/delivery/http-controllers"
 	"github.com/rishad004/learning-platform-go/user-service/internal/repository"
 	"github.com/rishad004/learning-platform-go/user-service/internal/service"
 	"github.com/rishad004/learning-platform-go/user-service/pkg"
@@ -20,19 +25,34 @@ func main() {
 	repo := repository.NewUserRepository(Db)
 	userService := service.NewUserService(repo)
 	userHandler := grpchandlers.NewUserHandler(userService)
+	httpController := httpcontrollers.NewUserController(userService)
 
 	g := grpc.NewServer()
 	pb.RegisterUserServiceServer(g, userHandler)
 
 	reflection.Register(g)
 
-	listen, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		log.Fatal("failed to listen, err: ", err)
-	}
+	go func() {
+		listen, err := net.Listen("tcp", ":8081")
+		if err != nil {
+			log.Fatal("failed to listen, err: ", err)
+		}
 
-	log.Println("Server listening on port :8080")
-	if err := g.Serve(listen); err != nil {
-		log.Fatal("failed to listen - err: ", err)
-	}
+		log.Println("Server listening on port :8081")
+		if err := g.Serve(listen); err != nil {
+			log.Fatal("failed to listen - err: ", err)
+		}
+	}()
+
+	go func() {
+		r := gin.Default()
+		httpController.UserRouters(r)
+
+		if err := r.Run(":8080"); err != nil {
+			log.Fatal("failed to listen, err: ", err)
+		}
+	}()
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
 }
